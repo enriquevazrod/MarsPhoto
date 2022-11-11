6155f58279aff27b8991eb305f6b3cd0fb3aa2fd
Enrique Vázquez Rodríguez <45948551+enriquevazrod@users.noreply.github.com>
2022-11-11 09:40:30 -0600

    Button test

--- app/src/main/java/com/evr/mpe/MainActivity.kt
+++ app/src/main/java/com/evr/mpe/MainActivity.kt
@@ -2,11 +2,20 @@ package com.evr.mpe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.buttonTest)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
